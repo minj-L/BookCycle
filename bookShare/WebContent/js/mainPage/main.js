5d95fa9a92544ab7293f28c2c7c5cafc7be39985
minj- <minj-@DESKTOP-417TCN2>
2022-09-28 00:24:49 +0900

    [chore] 주석삭제, 캐로셀 hover 작업중

--- bookShare/WebContent/js/mainPage/main.js
+++ bookShare/WebContent/js/mainPage/main.js
@@ -3,65 +3,79 @@
  */
 
 //테이블에 마우스 올라가면 엘리먼트 강조
-$(function(){
-	$('#ourUsedBook_1').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+$(function() {
+	$('#ourUsedBook_1').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_1').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_1').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_2').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_2').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_2').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_2').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_3').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_3').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_3').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_3').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_4').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_4').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_4').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_4').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_5').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_5').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_5').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_5').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_6').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_6').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_6').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_6').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_7').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_7').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_7').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_7').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_8').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_8').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_8').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_8').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_9').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_9').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_9').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_9').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-	$('#ourUsedBook_10').mouseover(function(){
-		$(this).css('background','#D8D8D8');
+	$('#ourUsedBook_10').mouseover(function() {
+		$(this).css('background', '#D8D8D8');
 	})
-	$('#ourUsedBook_10').mouseout(function(){
-		$(this).css('background','#ededed');
+	$('#ourUsedBook_10').mouseout(function() {
+		$(this).css('background', '#ededed');
 	})
-})
+});
+
+//캐로셀 cell hover 주기
+$(".cell_1").hover(
+	function() {
+		$(".cell_1").animate({
+			width: "40px" //커질크기
+		}, 300); //속도(slow, fast, 숫자...)
+	},
+	function() {
+		$(".cell_1").animate({
+			width: "32px" //원래크기
+		}, 300); //속도(slow, fast, 숫자...)
+	}
+)
